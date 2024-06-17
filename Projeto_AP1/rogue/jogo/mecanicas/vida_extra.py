6e1235a9eb1c07fba8a554111f580d2f61c99caf
BarbaraMalta <161264228+BarbaraMalta@users.noreply.github.com>
2024-06-16 23:22:15 -0300

    vida_extra.py

--- Projeto_AP1/rogue/jogo/mecanicas/vida_extra.py
+++ Projeto_AP1/rogue/jogo/mecanicas/vida_extra.py
@@ -1,1 +1,21 @@
+class vida_extra:
+    def desenhar_simbolo(self):
+        return "$"
+    
+    def __init__(self):
+        while True:
+            x = random.randint(0, 9)
+            y = random.randint(0, 9)
+            if not (x == y == 0) and not (x == y == 9):
+                break
 
+        self.posicao = [x, y]
+        self.visível = True
+        
+    def vida_extra(self, aventureiro):
+        if aventureiro["posicao"] == vida_extra:
+        aventureiro["vidas_extras"] += 1
+        vida_extra.clear()
+        print("Você ganhou uma vida extra!")
+
+        self.visível = False    
